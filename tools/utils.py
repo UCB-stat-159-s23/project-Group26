5904335cf1ab4202b81622bc857327c56fe6ec6d
Yiran Li <lyiran11@gmail.com>
2023-05-11 03:14:21 +0000

    added utility functions

---
+++ tools/utils.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import seaborn as sns
+
+# function to get the total number of counts of evictions per selected feature
+def get_count_plot(df, feature):
+    """
+    This function aims to get the total counts of evictions for each category in a certain feature, and
+    display the result in the form of a barplot.
+    """
+    # first get the data frame for the value counts
+    counts_df = df[[feature]].value_counts().reset_index(name='counts')
+
+    # specify the values for x-axis and y-axis
+    x = list(counts_df[feature])
+    y = list(counts_df['counts'])
+
+    # plot the barplot
+    fig, ax = plt.subplots()
+    fig.set_figwidth(20)
+    plt.setp(ax.get_xticklabels(), rotation=45, horizontalalignment='right', fontsize=8)
+    plt.xlabel(feature, labelpad = 10)
+    plt.ylabel('counts')
+    plt.title("Number of Evictions per " + feature)
+    plt.bar(x, y)
+
+
+# function to get the total number of the reasons for eviction and plot it
+def reason_for_evict(self, df, names):
+    """
+    This function aims to get the total number of the reasons for eviction for each different reason,
+    and display the result in the form of a barplot.
+    The arugument 'names' is a list of the names for certain eviction reasons that you want to look into.
+    """
+    self.df = df
+    self.names = names
+    reasons = df[names]
+    self.reasons = reasons
+    columnNames = reasons.columns
+
+    # create an empty dictionary for the reasons that has TRUE input for future counts
+    trues_oc = {}
+
+    # get the total number of TRUE observations for each reason of eviction and print it 
+    for i in reasons.columns:
+        trues_oc[i] = reasons[i].sum()
+    print(trues_oc)
+
+    # make it into a dataframe for plotting purpose
+    argh = pd.DataFrame(trues_oc, ['True'], columnNames).reset_index()
+    #print(argh)
+
+    # print the result
+    reasons_list = [column for column in argh.columns]
+    #print(reasons_list)
+    reasons_bar = argh.plot(figsize=(8,5), x='index', y=reasons_list[1:], kind='bar', rot=0)
+    plt.xlabel('Reasons', labelpad = 10)
+    plt.ylabel('counts')
+    plt.title("Reasons count for Different Reasons for Eviction")
+    plt.legend(loc='best', fontsize='7')
